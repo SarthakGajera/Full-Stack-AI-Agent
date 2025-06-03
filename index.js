66169a2963f9d36f49c09234b92b10b4e7f78841
SarthakGajera <sarthakgajera91@gmail.com>
2025-06-03 10:31:50 +0530

    MongoDb connection

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("mongodb connected");
+    app.listen(PORT, () => console.log("server at http://localhost:3000"));
+  })
+  .catch((err) => console.error("MongoDb error", err));
