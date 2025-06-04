930b99b8bb66e3da43429e6ce48e2d6662969922
SarthakGajera <sarthakgajera91@gmail.com>
2025-06-04 18:33:08 +0530

    user routes

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes.js";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", userRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
